d93b339201842e2d042b6826655179d8d9154c13
filipamanovic91 <filipamanovic91@gmail.com>
2020-07-16 11:17:10 +0200

    AddedLogFolder

---
+++ EF_Commands/Log/EF_LogUseCase.cs
@@ -0,0 +1,31 @@
+﻿using Application.Interfaces;
+using Bogus.DataSets;
+using EF_DataAccess;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EF_Commands.Log
+{
+    public class EF_LogUseCase : EF_BaseEntity, ILogUseCase
+    {
+        public EF_LogUseCase(asp_projectContext context) : base(context)
+        {
+        }
+
+        public void Log(IUseCase useCase, IApplicationActor actor, object useCaseData)
+        {
+            Context.UseCaseLogs.Add(new Domain.UseCaseLog
+            {
+                Actor = actor.Identity,
+                Data = JsonConvert.SerializeObject(useCaseData),
+                CreatedAt = DateTime.UtcNow,
+                UseCaseName = useCase.UseCaseName,
+                Timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds()
+            });
+
+            Context.SaveChanges();
+        }
+    }
+}
---
+++ EF_Commands/Log/LogToFileUseCase.cs
@@ -0,0 +1,23 @@
+﻿using Application.Interfaces;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace EF_Commands.Log
+{
+    public class LogToFileUseCase : ILogUseCase
+    {
+        public void Log(IUseCase useCase, IApplicationActor actor, object useCaseData)
+        {
+            using (System.IO.StreamWriter file =
+            new System.IO.StreamWriter(@"..\EF_Commands\Log\UseCaseLog.txt", true))
+            {
+                file.WriteLine($"User: {actor.Identity}, executed command " +
+                    $"{useCase.UseCaseName} at {DateTime.Now}" +
+                    $"{JsonConvert.SerializeObject(useCaseData)}");
+            }
+        }
+    }
+}
